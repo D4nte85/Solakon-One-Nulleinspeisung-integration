e170b76d2658071f7c6d757114819bd6318d0124
D4nte85 <eike_85@freenet.de>
2026-04-04 11:21:53 +0200

    Update config_flow.py

--- custom_components/solakon_nulleinspeisung/config_flow.py
+++ custom_components/solakon_nulleinspeisung/config_flow.py
@@ -105,14 +105,3 @@ class SolakonOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, entry: config_entries.ConfigEntry) -> None:
         self._entry = entry
-
-    async def async_step_init(
-        self, user_input: dict | None = None
-    ) -> config_entries.FlowResult:
-        current = {**self._entry.data, **self._entry.options}
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-        return self.async_show_form(
-            step_id="init",
-            data_schema=_schema(current),
-        )
